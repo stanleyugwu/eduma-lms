347b7d42b983877c3ee6dc24521d3b6af7592233
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-09-18 11:44:11 +0100

    ✨ initialize project

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+// Site URL
+const SITE_URL = 'https://test.thimpress.com';
+
+// List products In App Purchases
+const PRODUCTS_IAP = ['4248', '4271', '4287'];
+
+const COPYRIGHTS = 'Eduma app 2022. All rights reserved';
+
+const CODE_PUSH = {
+  ios: '',
+  android: '',
+};
+
+export { SITE_URL, PRODUCTS_IAP, COPYRIGHTS, CODE_PUSH };
